7a63cf4188b0f1dc69fd218cf44fe42f41e4621b
GeorgeTedeev <biruang123@gmail.com>
2020-08-18 16:34:52 +0300

    init

---
+++ src/main/java/com/project/offer/tasksetter/TaskSetterApplication.java
@@ -0,0 +1,13 @@
+package com.project.offer.tasksetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSetterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/offer/tasksetter/TaskSetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.offer.tasksetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
